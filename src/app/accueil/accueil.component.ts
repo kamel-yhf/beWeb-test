94bca1e4e203f36443e1888a0b18ebba3c7b8120
davids-pro <54986025+davids-pro@users.noreply.github.com>
2019-11-27 16:40:58 +0100

    backup

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,5 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import * as $ from 'jquery';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA
+} from '@angular/material/dialog';
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-accueil',
@@ -7,11 +17,41 @@ import * as $ from 'jquery';
   styleUrls: [ './accueil.component.scss' ]
 })
 export class AccueilComponent implements OnInit {
-  constructor() {}
+  animal: string;
+  name: string;
+
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit() {}
 
   hide() {
     $('#proot').slideToggle('slow');
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogBewebComponent, {
+      width: '250px',
+      data: { name: this.name, animal: this.animal }
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
+}
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: 'dialog.html'
+})
+export class DialogBewebComponent {
+  constructor(
+    public dialogRef: MatDialogRef<DialogBewebComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
